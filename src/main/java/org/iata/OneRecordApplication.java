af387599672f6339d74155c6693fcb072fd1fc03
Andra Blaj <blaja@iata.org>
2021-04-09 10:02:42 +0200

    Add client resource for calling other ONE Record Servers

--- src/main/java/org/iata/OneRecordApplication.java
+++ src/main/java/org/iata/OneRecordApplication.java
@@ -1,9 +1,16 @@
 package org.iata;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.logging.LogManager;
 
@@ -15,6 +22,8 @@ import java.util.logging.LogManager;
 })
 public class OneRecordApplication extends SpringBootServletInitializer {
 
+  private static final Logger LOG = LoggerFactory.getLogger(OneRecordApplication.class);
+
   public static void main(String[] args) {
     LogManager.getLogManager().reset();
     SLF4JBridgeHandler.removeHandlersForRootLogger();
@@ -22,6 +31,30 @@ public class OneRecordApplication extends SpringBootServletInitializer {
 
     new OneRecordApplication().configure(new SpringApplicationBuilder(OneRecordApplication.class))
         .registerShutdownHook(true).application().run(args);
+  }
+
+  @Bean
+  public RestTemplate getRestTemplate() {
+    RestTemplate restTemplate = new RestTemplate();
+    HttpComponentsClientHttpRequestFactory requestFactory = null;
+
+    try {
+      HttpClient httpClient = HttpClients.custom()
+          .setMaxConnTotal(5)
+          .setMaxConnPerRoute(5)
+          .build();
+
+      requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+      requestFactory.setReadTimeout(10000);
+      requestFactory.setConnectTimeout(10000);
+
+      restTemplate.setRequestFactory(requestFactory);
+    } catch (Exception exception) {
+      LOG.error("Exception occurred while creating restTemplate " + exception);
+      exception.printStackTrace();
+    }
+
+    return restTemplate;
   }
 
 }
---
+++ src/main/java/org/iata/resource/OneRecordClientResource.java
@@ -0,0 +1,51 @@
+package org.iata.resource;
+
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.inject.Inject;
+import java.net.URI;
+import java.util.Objects;
+
+/**
+ * Resource used to call an external ONE Record Server
+ *
+ * @author blaja
+ */
+@RestController
+@RequestMapping(value="/client")
+public class OneRecordClientResource {
+
+  private static final Logger LOG = LoggerFactory.getLogger(OneRecordClientResource.class);
+
+  private final RestTemplate restTemplate;
+  private final Environment env;
+
+  @Inject
+  public OneRecordClientResource(RestTemplate restTemplate, Environment env) {
+    this.restTemplate = restTemplate;
+    this.env = env;
+  }
+
+  @RequestMapping(value = "/server-data", method = RequestMethod.GET)
+  @ApiOperation(value = "Retrieves data from another ONE Record Server")
+  public String getServerData() {
+    LOG.info("Got inside server-data method");
+    try {
+      String serverEndpoint = env.getProperty("endpoint.one-record-server-service");
+      LOG.info("One Record Server Endpoint name : [" + serverEndpoint + "]");
+
+      return restTemplate.getForObject(new URI(Objects.requireNonNull(serverEndpoint)), String.class);
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+    return "Exception occurred.. so, returning default data";
+  }
+
+}

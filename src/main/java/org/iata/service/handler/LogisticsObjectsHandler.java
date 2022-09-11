52d81d3a6bd96793179bbe3800af7ea890069547
Daniel A. Döppner <daniel.doeppner>
2022-09-11 21:14:26 +0200

    added pub/sub for POST requests, i.e. notify subscribers when new LogisticsObjects are created

--- src/main/java/org/iata/service/handler/LogisticsObjectsHandler.java
+++ src/main/java/org/iata/service/handler/LogisticsObjectsHandler.java
@@ -1,5 +1,6 @@
 package org.iata.service.handler;
 
+import cz.cvut.kbss.jopa.model.annotations.OWLClass;
 import org.iata.api.model.AuditTrail;
 import org.iata.api.model.LogisticsObjectRef;
 import org.iata.api.model.Memento;
@@ -8,101 +9,111 @@ import org.iata.api.model.Timemap;
 import org.iata.cargo.model.LogisticsObject;
 import org.iata.model.AccessControlList;
 import org.iata.model.enums.EventType;
-import org.iata.service.AccessControlListService;
-import org.iata.service.AuditTrailsService;
-import org.iata.service.LogisticsObjectsService;
-import org.iata.service.SubscriptionsService;
-import org.iata.service.VersioningService;
+import org.iata.service.*;
 import org.iata.util.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.wc.acl.model.Authorization;
 
 import javax.inject.Inject;
+import java.lang.annotation.Annotation;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
 @Service
 public class LogisticsObjectsHandler {
 
-  private final LogisticsObjectsService logisticsObjectsService;
-  private final AuditTrailsService auditTrailsService;
-  private final AccessControlListService accessControlListService;
-  private final VersioningService versioningService;
+    private final LogisticsObjectsService logisticsObjectsService;
+    private final AuditTrailsService auditTrailsService;
+    private final AccessControlListService accessControlListService;
+    private final VersioningService versioningService;
 
-  private final SubscriptionsService subscriptionsService;
+    private final SubscriptionsService subscriptionsService;
 
-  @Inject
-  public LogisticsObjectsHandler(LogisticsObjectsService logisticsObjectsService,
-                                 AuditTrailsService auditTrailsService,
-                                 AccessControlListService accessControlListService,
-                                 VersioningService versioningService,
-                                 SubscriptionsService subscriptionsService) {
-    this.logisticsObjectsService = logisticsObjectsService;
-    this.auditTrailsService = auditTrailsService;
-    this.accessControlListService = accessControlListService;
-    this.versioningService = versioningService;
-    this.subscriptionsService = subscriptionsService;
-  }
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogisticsObjectsHandler.class);
 
-  public LogisticsObject handleAddLogisticsObject(LogisticsObject logisticsObject, String companyIdentifier) {
-    logisticsObject.setCompanyIdentifier(logisticsObject.getCompanyIdentifier());
-    String loid = companyIdentifier + "/" + logisticsObject.getClass().getSimpleName() + "_" + Utils.getRandomNumberString();
-    logisticsObject.setId(loid);
-    logisticsObjectsService.addLogisticsObject((logisticsObject));
+    @Inject
+    public LogisticsObjectsHandler(LogisticsObjectsService logisticsObjectsService,
+                                   AuditTrailsService auditTrailsService,
+                                   AccessControlListService accessControlListService,
+                                   VersioningService versioningService,
+                                   SubscriptionsService subscriptionsService) {
+        this.logisticsObjectsService = logisticsObjectsService;
+        this.auditTrailsService = auditTrailsService;
+        this.accessControlListService = accessControlListService;
+        this.versioningService = versioningService;
+        this.subscriptionsService = subscriptionsService;
+    }
 
-    // Save create object in the audit trail
-    saveAuditTrailForLo(logisticsObject, loid);
+    public LogisticsObject handleAddLogisticsObject(LogisticsObject logisticsObject, String companyIdentifier) {
+        logisticsObject.setCompanyIdentifier(logisticsObject.getCompanyIdentifier());
+        String loid = companyIdentifier + "/" + logisticsObject.getClass().getSimpleName() + "_" + Utils.getRandomNumberString();
+        logisticsObject.setId(loid);
+        logisticsObjectsService.addLogisticsObject((logisticsObject));
 
-    // Create ACL
-    createAclForLo(loid);
+        // Save create object in the audit trail
+        saveAuditTrailForLo(logisticsObject, loid);
 
-    // Create Timemap
-    createTimemapForLo(loid);
+        // Create ACL
+        createAclForLo(loid);
 
-    return logisticsObject;
-  }
+        // Create Timemap
+        createTimemapForLo(loid);
 
-  private void saveAuditTrailForLo(LogisticsObject logisticsObject, String loid) {
-    AuditTrail auditTrail = new AuditTrail();
-    String auditTrailId = loid + "/auditTrail";
-    auditTrail.setId(auditTrailId);
-    Memento initialLo = new Memento();
+        // Notify Subscribers
+        Annotation logisticsObjectClassAnnotation = Arrays.stream(logisticsObject.getClass().getDeclaredAnnotations()).filter(a -> a.annotationType() == OWLClass.class).findAny().orElse(null);
+        if (logisticsObjectClassAnnotation != null && logisticsObjectClassAnnotation.getClass() == OWLClass.class) {
+            subscriptionsService.notifySubscribers(EventType.OBJECT_CREATED, ((OWLClass) logisticsObjectClassAnnotation).iri(), logisticsObject.getId());
+        } else {
+            LOGGER.error("Could not identify IRI of LogisticsObject {}", logisticsObject.getId());
+        }
 
-    // TODO fonctionality for creating snapshots/mementos
-    initialLo.setId(auditTrailId + "/loInitialSnapshot");
-    auditTrail.setLoInitialSnapshot(initialLo);
-    LogisticsObjectRef logisticsObjectRef = new LogisticsObjectRef();
-    logisticsObjectRef.setLogisticsObjectId(loid);
-    auditTrail.setLogisticsObjectRef(logisticsObjectRef);
-    auditTrailsService.addAuditTrail(auditTrail);
-  }
+        return logisticsObject;
+    }
 
-  private void createAclForLo(String loid) {
-    // TODO
-    AccessControlList accessControlList = new AccessControlList();
-    accessControlList.setId(loid + "/acl");
-    accessControlList.setLogisticsObjectRef(loid);
-    List<Authorization> authorizationList = new ArrayList<>();
-    Authorization authorization = new Authorization();
-    authorization.setId(loid + "/acl/authorization_1");
-    authorizationList.add(authorization);
-    accessControlList.setAuthorizations(new HashSet<>(authorizationList));
-    accessControlListService.addAccessControlList(accessControlList);
-  }
+    private void saveAuditTrailForLo(LogisticsObject logisticsObject, String loid) {
+        AuditTrail auditTrail = new AuditTrail();
+        String auditTrailId = loid + "/auditTrail";
+        auditTrail.setId(auditTrailId);
+        Memento initialLo = new Memento();
 
-  private void createTimemapForLo(String loid) {
-    Timemap timemap = new Timemap();
-    timemap.setId(loid + "/timemap");
-    timemap.setOriginal(loid);
-    timemap.setTimegate(loid + "/timegate");
-    versioningService.addTimemap(timemap);
-  }
+        // TODO fonctionality for creating snapshots/mementos
+        initialLo.setId(auditTrailId + "/loInitialSnapshot");
+        auditTrail.setLoInitialSnapshot(initialLo);
+        LogisticsObjectRef logisticsObjectRef = new LogisticsObjectRef();
+        logisticsObjectRef.setLogisticsObjectId(loid);
+        auditTrail.setLogisticsObjectRef(logisticsObjectRef);
+        auditTrailsService.addAuditTrail(auditTrail);
+    }
 
-  public void handleUpdateLogisticsObject(PatchRequest patchRequest) {
-    logisticsObjectsService.updateLogisticsObject(patchRequest);
-    auditTrailsService.updateAuditTrail(patchRequest);
-    subscriptionsService.notifySubscribers(EventType.OBJECT_UPDATED, patchRequest.getLogisticsObjectRef().getLogisticsObjectType(), patchRequest.getLogisticsObjectRef().getLogisticsObjectId());
-  }
+    private void createAclForLo(String loid) {
+        // TODO
+        AccessControlList accessControlList = new AccessControlList();
+        accessControlList.setId(loid + "/acl");
+        accessControlList.setLogisticsObjectRef(loid);
+        List<Authorization> authorizationList = new ArrayList<>();
+        Authorization authorization = new Authorization();
+        authorization.setId(loid + "/acl/authorization_1");
+        authorizationList.add(authorization);
+        accessControlList.setAuthorizations(new HashSet<>(authorizationList));
+        accessControlListService.addAccessControlList(accessControlList);
+    }
+
+    private void createTimemapForLo(String loid) {
+        Timemap timemap = new Timemap();
+        timemap.setId(loid + "/timemap");
+        timemap.setOriginal(loid);
+        timemap.setTimegate(loid + "/timegate");
+        versioningService.addTimemap(timemap);
+    }
+
+    public void handleUpdateLogisticsObject(PatchRequest patchRequest) {
+        logisticsObjectsService.updateLogisticsObject(patchRequest);
+        auditTrailsService.updateAuditTrail(patchRequest);
+        subscriptionsService.notifySubscribers(EventType.OBJECT_UPDATED, patchRequest.getLogisticsObjectRef().getLogisticsObjectType(), patchRequest.getLogisticsObjectRef().getLogisticsObjectId());
+    }
 
 }
